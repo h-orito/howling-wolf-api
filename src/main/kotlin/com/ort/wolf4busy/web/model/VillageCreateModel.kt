7f0489f20efaf3d9807116465ae18c80e31a45c4
h-orito <16453967+h-orito@users.noreply.github.com>
2019-03-31 22:34:50 +0900

    実装中

---
+++ src/main/kotlin/com/ort/wolf4busy/web/model/VillageCreateModel.kt
@@ -0,0 +1,4 @@
+package com.ort.wolf4busy.web.model
+
+class VillageCreateModel {
+}
---
+++ src/main/kotlin/com/ort/wolf4busy/web/model/VillageListModel.kt
@@ -0,0 +1,8 @@
+package com.ort.wolf4busy.web.model
+
+import com.ort.wolf4busy.web.model.inner.VillageListVillageModel
+
+data class VillageListModel(
+        val villageList: List<VillageListVillageModel>
+) {
+}
