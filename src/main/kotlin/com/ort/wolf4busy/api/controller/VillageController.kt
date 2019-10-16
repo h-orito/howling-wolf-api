acf2b3f2e215ebe5f717dbbdfab1a977bccfd6d4
h-orito <16453967+h-orito@users.noreply.github.com>
2019-10-16 17:30:24 +0900

    村画面API

--- src/main/kotlin/com/ort/wolf4busy/api/controller/VillageController.kt
+++ src/main/kotlin/com/ort/wolf4busy/api/controller/VillageController.kt
@@ -1,8 +1,11 @@
 package com.ort.wolf4busy.api.controller
 
 import com.ort.wolf4busy.api.view.village.VillageListView
+import com.ort.wolf4busy.api.view.village.VillageMessageView
 import com.ort.wolf4busy.api.view.village.VillageView
+import com.ort.wolf4busy.application.coordinator.MessageCoordinator
 import com.ort.wolf4busy.application.service.VillageService
+import com.ort.wolf4busy.domain.model.village.message.VillageMessages
 import com.ort.wolf4busy.fw.security.Wolf4busyUser
 import org.slf4j.LoggerFactory
 import org.springframework.security.core.annotation.AuthenticationPrincipal
@@ -14,7 +17,8 @@ import org.springframework.web.bind.annotation.RestController
 @CrossOrigin
 @RestController
 class VillageController(
-        val villageService: VillageService
+        val villageService: VillageService,
+        val messageCoordinator: MessageCoordinator
 ) {
 
     private val logger = LoggerFactory.getLogger(VillageController::class.java)
@@ -33,6 +37,19 @@ class VillageController(
         val village = villageService.findVillage(villageId)
         return VillageView(
                 village = village
+        )
+    }
+
+    @GetMapping("/village/{villageId}/day/{day}/time/{time}/message-list")
+    fun message(
+            @PathVariable("villageId") villageId: Int,
+            @PathVariable("day") day: Int,
+            @PathVariable("time") time: String,
+            @AuthenticationPrincipal user: Wolf4busyUser?
+    ): VillageMessageView {
+        val messages: VillageMessages = messageCoordinator.findMessageList(villageId, day, time, user)
+        return VillageMessageView(
+                messageList = messages.messageList
         )
     }
 }
