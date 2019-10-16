fa1edc3a09504935719cdf02a9b90fc9c52a0950
h-orito <16453967+h-orito@users.noreply.github.com>
2019-10-16 17:06:24 +0900

    JWT検証はfirebaseのライブラリを使用する

--- src/main/kotlin/com/ort/wolf4busy/Wolf4busyApplication.kt
+++ src/main/kotlin/com/ort/wolf4busy/Wolf4busyApplication.kt
@@ -1,6 +1,7 @@
 package com.ort.wolf4busy
 
 import com.ort.dbflute.allcommon.DBFluteBeansJavaConfig
+import com.ort.wolf4busy.fw.config.FirebaseConfig
 import com.ort.wolf4busy.fw.config.Wolf4busyAppConfig
 import com.ort.wolf4busy.fw.config.Wolf4busyWebMvcConfigurer
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.Import
 
 
 @SpringBootApplication
-@Import(DBFluteBeansJavaConfig::class, Wolf4busyAppConfig::class, Wolf4busyWebMvcConfigurer::class)
+@Import(DBFluteBeansJavaConfig::class, Wolf4busyAppConfig::class, Wolf4busyWebMvcConfigurer::class, FirebaseConfig::class)
 class Wolf4busyApplication
 
 fun main(args: Array<String>) {
