1e387e8d6f8b5050a65493de67f71bcf3491ecd5
h-orito <16453967+h-orito@users.noreply.github.com>
2020-01-08 23:20:05 +0900

    add junit test

--- src/main/kotlin/com/ort/wolf4busy/domain/model/village/action/VillageSaySituation.kt
+++ src/main/kotlin/com/ort/wolf4busy/domain/model/village/action/VillageSaySituation.kt
@@ -10,20 +10,34 @@ import com.ort.wolf4busy.domain.model.village.participant.VillageParticipant
 data class VillageSaySituation(
     val isAvailableSay: Boolean,
     val selectableMessageTypeList: List<VillageSayMessageTypeSituation> = listOf(),
-    val selectableFaceTypeList: List<CharaFace>
+    val selectableFaceTypeList: List<CharaFace>,
+    val defaultMessageType: MessageType?
 ) {
     constructor(
         village: Village,
         participant: VillageParticipant?,
         charas: Charas,
-        latestDayMessageList: List<Message>
+        latestDayMessageList: List<Message> // TODO これ通常発言だけになってない？
     ) : this(
         isAvailableSay = isAvailableSay(village, participant),
         selectableMessageTypeList = getSelectableMessageTypeList(village, participant, latestDayMessageList),
-        selectableFaceTypeList = getSelectableFaceTypeList(village, participant, charas)
+        selectableFaceTypeList = getSelectableFaceTypeList(village, participant, charas),
+        defaultMessageType = detectDefaultMessageType(
+            isAvailableSay(village, participant),
+            getSelectableMessageTypeList(village, participant, latestDayMessageList)
+        )
     )
 
     companion object {
+
+        private val defaultMessageTypeOrder = listOf(
+            CDef.MessageType.人狼の囁き,
+            CDef.MessageType.共鳴発言,
+            CDef.MessageType.通常発言,
+            CDef.MessageType.死者の呻き,
+            CDef.MessageType.見学発言,
+            CDef.MessageType.独り言
+        )
 
         // ===================================================================================
         //                                                            Constructor Assist Logic
@@ -32,9 +46,8 @@ data class VillageSaySituation(
             // 参加していない場合はNG
             participant ?: return false
             // 突然死した場合はエピローグ以外NG
-            if (participant.dead != null //
-                && CDef.DeadReason.codeOf(participant.dead.code) == CDef.DeadReason.突然 //
-                && CDef.VillageStatus.codeOf(village.status.code) != CDef.VillageStatus.エピローグ
+            if (participant.dead?.toCdef() == CDef.DeadReason.突然 //
+                && village.status.toCdef() != CDef.VillageStatus.エピローグ
             ) {
                 return false
             }
@@ -113,6 +126,19 @@ data class VillageSaySituation(
                 village.participant.memberList.first { it.id == participant.id }.charaId
             }
             return charas.list.first { it.id == charaId }.faceList
+        }
+
+        private fun detectDefaultMessageType(
+            availableSay: Boolean,
+            selectableMessageTypeList: List<VillageSayMessageTypeSituation>
+        ): MessageType? {
+            if (!availableSay || selectableMessageTypeList.isEmpty()) return null
+            val selectableMessageTypeList = selectableMessageTypeList.map { it.messageType.toCdef() }
+
+            defaultMessageTypeOrder.forEach { cdefMessageType ->
+                if (selectableMessageTypeList.contains(cdefMessageType)) return MessageType(cdefMessageType)
+            }
+            return null
         }
     }
 }
